7a34ff3abd0367d03265fbd92734eb7f32546b72
Hollie <holliemount11@hotmail.com>
2025-01-14 09:43:58 +0000

    Feat: add logic to implement recyclerview

--- app/src/main/java/com/example/litsaandroid/ui/mainActivity/MainActivityViewModel.java
+++ app/src/main/java/com/example/litsaandroid/ui/mainActivity/MainActivityViewModel.java
@@ -4,10 +4,23 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.litsaandroid.model.Places;
+import com.example.litsaandroid.model.PlacesRepository;
+
+import java.util.List;
 
 public class MainActivityViewModel extends AndroidViewModel {
 
+    private PlacesRepository placesRepository;
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
+        this.placesRepository = new PlacesRepository(application);
+    }
+
+    public LiveData<List<Places>> getAllPlaces(){
+        return placesRepository.getMutableLiveData();
+        //Note: to complete getMutableLiveData method in PlacesRepository when have Retrofit instance
     }
 }
