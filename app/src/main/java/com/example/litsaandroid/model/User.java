2de859edb792ae7aef4bdc02aecb355c4c8123cf
Maria Santos <mariaeugenia.fs@hotmail.com>
2025-01-17 12:15:22 +0000

    refactor: User now extends BaseObservable and implements Parcelable

--- app/src/main/java/com/example/litsaandroid/model/User.java
+++ app/src/main/java/com/example/litsaandroid/model/User.java
@@ -1,6 +1,15 @@
 package com.example.litsaandroid.model;
 
-public class User {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.litsaandroid.BR;
+
+public class User extends BaseObservable implements Parcelable {
 
     private String name;
     private String email;
@@ -15,27 +24,64 @@ public class User {
         this.password = password;
     }
 
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+        notifyPropertyChanged(BR.email);
     }
 
+    @Bindable
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+        notifyPropertyChanged(BR.password);
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(email);
+        dest.writeString(name);
+        dest.writeString(password);
+    }
+
+    protected User(Parcel in) {
+    name = in.readString();
+    email= in.readString();
+    password = in.readString();
+    }
+
+    private static final Creator<User> CREATOR = new Creator<User>() {
+
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }
