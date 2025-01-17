0bce280a184f4bd4baab7a0c62459028d7465191
Maria Santos <mariaeugenia.fs@hotmail.com>
2025-01-17 12:23:53 +0000

    "Created a User View Model"

---
+++ app/src/main/java/com/example/litsaandroid/User/UserViewModel.java
@@ -0,0 +1,34 @@
+package com.example.litsaandroid.User;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.litsaandroid.model.Places;
+import com.example.litsaandroid.model.PlacesRepository;
+import com.example.litsaandroid.model.User;
+
+import java.util.List;
+
+public class UserViewModel extends AndroidViewModel {
+    private UserRepository userRepository;
+
+    public UserViewModel(@NonNull Application application) {
+        super(application);
+        this.userRepository = new UserRepository(application);
+    }
+
+    public LiveData<User> getUser(String token) {
+        return userRepository.getUser(token);
+    }
+    public void addUser (String email, String password, String username){
+    userRepository.addUser(email, password, username);
+    }
+    public void logUser(String email, String password){
+        userRepository.logUser(email,password);
+    }
+}
+
+
